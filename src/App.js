2b80be304f15c00e9cab9d0f7ee8dc177beb198a
Ivaylo-Kirov <ivo.kirov@gmail.com>
2019-09-02 15:39:27 -0400

    Initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Amplify, {API, graphqlOperation} from 'aws-amplify';
+import awsconfig from './aws-exports';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+Amplify.configure(awsconfig);
+
+const listTodos = `query listTodos {
+  listTodos{
+    items{
+      id
+      name
+      description
+    }
+  }
+}`
+
+const addTodo = `mutation createTodo($name:String! $description: String!) {
+  createTodo(input:{
+    name:$name
+    description:$description
+  }){
+    id
+    name
+    description
+  }
+}`
+
+class App extends React.Component {
+
+  todoMutation = async () => {
+    const todoDetails = {name: 'Second todo', description: 'wow'};
+    const newTodo = await API.graphql(graphqlOperation(addTodo, todoDetails));
+    console.log(JSON.stringify(newTodo));
+  }
+
+  listQuery = async () => {
+    console.log('listing todos');
+    const allTodos = await API.graphql(graphqlOperation(listTodos));
+    console.log(JSON.stringify(allTodos));
+  }
+
+
+  render() {
+    return(
+      <div>
+        <button onClick={this.listQuery}>GraphQL Query</button>
+        <button onClick={this.todoMutation}>GraphQL Mutation</button>
+      </div>
+    );
+  }
 }
 
 export default App;
